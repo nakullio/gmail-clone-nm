8607c3792a76f1c476ce074cd591054ddac3a58d
Nakula <nakulamarvellio@gmail.com>
2021-03-31 00:54:59 +0700

    ADD: Additional sidebarOptions

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,7 +4,12 @@ import './Sidebar.css';
 import AddIcon from '@material-ui/icons/Add';
 import InboxIcon from '@material-ui/icons/Inbox';
 import SidebarOption from './SidebarOption';
-import StarIcon from '@material-ui/icons/Star'
+import StarIcon from '@material-ui/icons/Star';
+import AccesTimeIcon from '@material-ui/icons/AccessTime';
+import LabelImportantIcon from '@material-ui/icons/LabelImportant';
+import NearMeIcon from '@material-ui/icons/NearMe';
+import NoteIcon from '@material-ui/icons/Note';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 function Sidebar() {
     return (
@@ -17,6 +22,11 @@ function Sidebar() {
 
             <SidebarOption Icon={InboxIcon} title="Inbox" number={54} selected={true}/>
             <SidebarOption Icon={StarIcon} title="Starred" number={54}/>
+            <SidebarOption Icon={AccesTimeIcon} title="Snoozed" number={54}/>
+            <SidebarOption Icon={LabelImportantIcon} title="Important" number={54}/>
+            <SidebarOption Icon={NearMeIcon} title="Sent" number={54}/>
+            <SidebarOption Icon={NoteIcon} title="Drafts" number={54}/>
+            <SidebarOption Icon={ExpandMoreIcon} title="More" number={54}/>
 
 
         </div>
