ab24b8b0ceb7f9f28dfa8895d47360b9be6345b5
Nakula <nakulamarvellio@gmail.com>
2021-03-31 22:04:28 +0700

    ADD: emailList settings

--- src/EmailList.js
+++ src/EmailList.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import {Checkbox , IconButton} from '@material-ui/core'
 import './EmailList.css'
+import ArrowDropDownIcon  from '@material-ui/icons/ArrowDropDown';
+import RedoIcon from '@material-ui/icons/Redo';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SettingsIcon from '@material-ui/icons/Settings';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
 
 function EmailList() {
     return (
         <div className="emailList">
-            
+            <div className="emailList__settings">
+                <div className="emailList__settingLeft">
+                    <Checkbox />
+                    <IconButton>
+                        <ArrowDropDownIcon />
+                    </IconButton>
+                    <IconButton>
+                        <RedoIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+
+                </div>
+                <div className="emailList__settingRight">
+                    <IconButton>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChevronRightIcon />
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardHideIcon />
+                    </IconButton>
+                    <IconButton>
+                        <SettingsIcon/>
+                    </IconButton>
+                </div>
+            </div>
         </div>
     )
 }
