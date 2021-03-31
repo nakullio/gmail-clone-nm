5fa964356605bb942db80a8b4ff0211132c43b98
Nakula <nakulamarvellio@gmail.com>
2021-03-31 21:46:35 +0700

    ADD: react-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import './App.css';
-import Header from './Header'
-import Sidebar from './Sidebar'
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Mail from './Mail';
+import EmailList from './EmailList'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Sidebar/>
-      
+    <Router>
+      <div className="App">
+        <Header />
 
-    </div>
+        <div className="app__body">
+        <Sidebar/>
+
+        <Switch>
+          <Route path="/mail">
+            <Mail />
+          </Route>
+          <Route path="/">
+            <EmailList />
+          </Route>
+
+        </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
