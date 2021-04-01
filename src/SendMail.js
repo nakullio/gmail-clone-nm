72e96b71b9ede6411cbc0cc15948ca3fac81f8c2
Nakula <nakulamarvellio@gmail.com>
2021-04-02 00:51:27 +0700

    ADD: email validation form

--- src/SendMail.js
+++ src/SendMail.js
@@ -26,7 +26,7 @@ function SendMail() {
                 <input 
                 name='to' 
                 placeholder="To" 
-                type="text" 
+                type="email" 
                 ref={register({ required: true})}/>
                 {errors.to && <p className="sendMail__error">To is Required!</p>}
 
