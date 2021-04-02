a99ce9f1aba17635b3cc2a83d5fbca0a11c81f65
Nakula <nakulamarvellio@gmail.com>
2021-04-02 21:55:42 +0700

    ADD: user Profile picture sync

--- src/Header.js
+++ src/Header.js
@@ -6,9 +6,13 @@ import SearchIcon from '@material-ui/icons/Search';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
 
 
 function Header() {
+    const user = useSelector(selectUser);
+
     return (
         <div className="header"> 
         <div className="header__left">
@@ -34,7 +38,7 @@ function Header() {
             <IconButton>
                 <NotificationsIcon />
             </IconButton>
-            <Avatar />
+            <Avatar src={user?.photoUrl} />
         </div>
 
         </div>
