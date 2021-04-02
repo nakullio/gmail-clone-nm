4ee7aff3ae34e8d3427954dfb632bca3b3dad523
Nakula <nakulamarvellio@gmail.com>
2021-04-02 21:48:54 +0700

    ADD: persistent login state features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Mail from './Mail';
 import EmailList from './EmailList'
-import SendMail from './SendMail'
+import SendMail from './SendMail';
+import { auth } from './firebase';
 
 import {
   BrowserRouter as Router,
@@ -13,14 +14,29 @@ import {
   Link
 } from "react-router-dom";
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { selectSendMessageIsOpen } from './features/mailSlice';
 import { selectUser } from './features/userSlice';
 import Login from './Login';
+import { login } from "./features/userSlice";
 
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
+  useEffect(()=> {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        // the user is logged in
+        dispatch(login({
+          displayName: user.displayName,
+          email: user.email,
+          photoUrl: user.photoURL
+        }))
+      }
+    })
+  }, [])
 
   return (
     <Router>
